eb78272e25f07bf72c1598691d5c6ffa467f0bfe
J9 <jeffbaltazar09@gmail.com>
2017-10-27 20:29:14 +0800

    Addtional Changes

--- platforms/browser/www/MK2.js
+++
@@ -1,7 +0,0 @@
- function RR() {
-	window.localStorage['MK2'] = document.getElementById('MK2').value;
-    document.getElementById('MK2').value=null;}
-
-    function RV() {
-        document.getElementById('MK2').value = window.localStorage['MK2'];
-    }
--- platforms/browser/www/mkL.js
+++
@@ -1,7 +0,0 @@
-function RR() {
-     window.localStorage['mkL'] = document.getElementById('mkL').value;
-    document.getElementById('mkL').value=null;}
-
-    function RV() {
-        document.getElementById('mkL').value = window.localStorage['mkL'];
-    }
